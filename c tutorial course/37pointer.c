72cb1ff1fcfaf94ec4845a71dc7fa780c4b415f4
Niraj Balpande <nirajbalpande12@gmail.com>
2025-04-11 17:41:10 +0530

    retouch

--- c tutorial course/37pointer.c
+++ c tutorial course/37pointer.c
@@ -6,9 +6,26 @@ int* ptr2 = NULL;
 printf("The Adress of pointer to a is %p\n",&ptra);  
 printf("The Adress of  a  %p\n",&a);
 printf("The Adress of a is %p\n",ptra);
-printf("The vaue of a is %d\n",*ptra);  
+printf("The vaue of a is %d\n",*ptra);//from this line we can change the value of a   
 printf("The value of a is %d\n",a);     
 printf("The Adress of pointer to a is %x\n",&ptra);   
 printf("The value of pointer ptr2 is %p\n",ptr2);  
 return 0;   
 }  
+
+/*
+🔚 Final Thoughts
+💡 What You’re Seeing:
+ptra holds the address of a.
+
+*ptra gives the value at that address.
+
+&ptra is the address of the pointer variable itself.
+
+%p → prints full address
+
+%x → prints raw hex format
+
+NULL → address 0 (ptr2 doesn't point anywhere)
+
+*/
